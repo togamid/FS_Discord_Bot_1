7b4d9aa4e40be1e8d73fa0f1b3fca808d7f9111c
togamid <gaisserto80670@th-nuernberg.de>
2020-10-23 23:36:54 +0200

    Adjust messages

--- src/main/java/EventListeners.java
+++ src/main/java/EventListeners.java
@@ -16,26 +16,27 @@ public class EventListeners extends ListenerAdapter {
             Guild guild = event.getGuild();
             MessageChannel channel = event.getChannel();
             Member member = event.getMember();
+            String mention = member.getUser().getAsMention();
             String roleName = msgContent.substring(msgContent.indexOf(' ')+1);
             if(roleName.isEmpty() || roleName.equals("!role")){
-                channel.sendMessage("Syntax: !role <Rollenname>").queue();
+                channel.sendMessage( mention + "Syntax: !role <Rollenname>").queue();
                 return;
             }
             List<Role> roles = guild.getRolesByName( roleName, true);
             if(roles.isEmpty()){
-                channel.sendMessage("Rolle nicht gefunden!").queue();
+                channel.sendMessage( mention +  " Rolle nicht gefunden!").queue();
                 return;
             }
             try {
                 if(member.getRoles().contains(roles.get(0))){
                     guild.removeRoleFromMember(member, roles.get(0)).queue();
-                    channel.sendMessage("Rolle erfolgreich entfernt!").queue();
+                    channel.sendMessage( mention + " Rolle \"" + roles.get(0).getName() + "\" erfolgreich entfernt!").queue();
                 } else {
                     guild.addRoleToMember(member, roles.get(0)).queue();
-                    channel.sendMessage("Rolle erfolgreich hinzugefügt!").queue();
+                    channel.sendMessage(mention + " Rolle \"" + roles.get(0).getName() + "\" erfolgreich hinzugefügt!").queue();
                 }
             } catch (HierarchyException e) {
-                channel.sendMessage("Du darfst diese Rolle nicht ändern!").queue();
+                channel.sendMessage(mention + " Du darfst diese Rolle nicht ändern!").queue();
             }
         }
     }
